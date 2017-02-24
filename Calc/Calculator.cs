a3b66806c0e1e079606128e7c7dd2dff9425b814
olzha <olzha@DESKTOP-QGB6P3L>
2017-02-24 18:01:04 +0600

    Добавьте файлы проекта.

---
+++ Calc/Calculator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    /// <summary>
+    /// My class for calc
+    /// </summary>
+    class Calculator
+    {
+        /// <summary>
+        /// dasdsadsd
+        /// </summary>
+        public double A { get; set; }
+
+        /// <summary>
+        /// dasdsadad
+        /// </summary>
+        public double B { get; set; }
+        
+        /// <summary>
+        /// dasdasdasdad
+        /// </summary>
+        /// <param name="operation">dasdasdasd</param>
+        /// <returns></returns>
+        public double Execute(string operation)
+        {
+            double res = 0;
+
+            if (operation == "+")
+            {
+                res = A + B;
+            }
+            else if (operation == "-")
+            {
+                res = A - B;
+            }
+            else if (operation == "/")
+            {
+                res = A / B;
+            }
+            else if (operation == "*")
+            {
+                res = A * B;
+            }
+            else if (operation == "!")
+            {
+                res = GetFact(Convert.ToInt32(A));
+            }
+            else if (operation == "div")
+            {
+                res = Math.Floor(A / B);
+            }
+            else if (operation == "mod")
+            {
+                res = GetFact(Convert.ToInt32(A));
+            }
+            else
+            {
+                throw new Exception("Operation not founded:" + operation);
+            }
+
+            return res;
+        }
+
+        /// <summary>
+        /// dasdadaasd
+        /// </summary>
+        /// <param name="val">dasdasdasdasd</param>
+        /// <returns></returns>
+        private double GetFact(int val)
+        {
+            double res = 1;
+
+            if (val > 1)
+            {
+                res *= val;
+                val--;
+                return GetFact(val) * res;
+            }
+            return res;
+        }
+
+    }
+}
---
+++ Calc/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isWork = true;
+
+            Calculator clc = new Calculator();
+            
+            while (isWork)
+            {
+
+                var a = GetValueFromConsole("Введите а");
+
+                var b = GetValueFromConsole("Введите Введите b");
+
+                var operation = GetValueFromConsole("Введите операцию: +, -, *, /");
+                
+                clc.A = Convert.ToDouble(a);
+                clc.B = Convert.ToDouble(b);
+                var res = clc.Execute(operation);
+
+                Console.WriteLine("Res: " + res);
+
+                var answer = GetValueFromConsole("Хотите закончить? (1 - да, 0 - нет)");
+                if (answer == "1")
+                {
+                    isWork = false;
+                }
+
+            }
+
+        }
+
+        static string GetValueFromConsole(string message)
+        {
+            Console.WriteLine("----------------------");
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        
+
+    }
+
+    
+}
